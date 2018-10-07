24ac017f26d8e7ccb2c301f338ffeb165d5ddc4e
xubin <xpin_1509@163.com>
2018-10-07 16:22:34 +0800

    create-react-app antd

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { DatePicker } from 'antd';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <DatePicker />
+      </div>
+    );
+  }
+}
+
+export default App;
